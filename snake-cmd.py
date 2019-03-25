a837e743b7255ffed7196fc5bdee1bc0cf2d62e6
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-03-25 22:06:20 +0000

    Create simple curses window

--- snake-cmd.py
+++ snake-cmd.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+import curses
+
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    stdscr.box()
+
+    stdscr.addstr(0, 0, "Snake")
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+curses.wrapper(main)
