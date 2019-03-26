4b191f1595e0a05004de161ddb879a698b070d4e
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-03-26 11:42:12 +0000

    Add Snake class and display initial state of snake

--- snake-cmd.py
+++ snake-cmd.py
@@ -1,5 +1,11 @@
 import curses
+import vectormath as vm
 
+
+class Snake:
+    def __init__(self):
+        self.cells = [vm.Vector2(curses.LINES // 2, curses.COLS // 2)]
+        self.direction = vm.Vector2(0, -1)
 
 def main(stdscr):
     # Clear screen
@@ -8,6 +14,15 @@ def main(stdscr):
     stdscr.box()
 
     stdscr.addstr(0, 0, "Snake")
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+    stdscr.clear()
+
+    snake = Snake()
+    for cell in snake.cells:
+        stdscr.addstr(int(cell.x), int(cell.y), "x")
 
     stdscr.refresh()
     stdscr.getkey()
