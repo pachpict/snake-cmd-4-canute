70b24e0afc59589bab9827a0c42163222975a509
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-03-26 21:31:49 +0000

    Make snake move every X seconds

--- snake-cmd.py
+++ snake-cmd.py
@@ -1,11 +1,27 @@
 import curses
 import vectormath as vm
+import threading
 
 
 class Snake:
     def __init__(self):
         self.cells = [vm.Vector2(curses.LINES // 2, curses.COLS // 2)]
         self.direction = vm.Vector2(0, -1)
+
+    def move(self):
+        for cell in self.cells:
+            cell += self.direction
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return
+
 
 def main(stdscr):
     # Clear screen
@@ -18,14 +34,19 @@ def main(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
-    stdscr.clear()
-
     snake = Snake()
-    for cell in snake.cells:
-        stdscr.addstr(int(cell.x), int(cell.y), "x")
 
-    stdscr.refresh()
-    stdscr.getkey()
+    def update():
+        snake.move()
 
+        stdscr.clear()
+
+        for cell in snake.cells:
+            stdscr.addstr(int(cell.x), int(cell.y), "x")
+
+        stdscr.refresh()
+        # stdscr.getkey()
+
+    set_interval(update, 0.5)
 
 curses.wrapper(main)
