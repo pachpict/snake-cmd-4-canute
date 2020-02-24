0d01b285ed7acfab6d717d71f0e62572d9287d3e
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-02-24 21:36:11 +0000

    Refactoring

--- snake-cmd.py
+++ snake-cmd.py
@@ -15,6 +15,28 @@ def main(stdscr):
     curses.curs_set(1)
 
     stdscr.clear()
+    show_title_screen(stdscr)
+
+    curses.curs_set(0)
+
+    score = show_game_screen(stdscr)
+    curses.curs_set(1)
+
+    stdscr.clear()
+    stdscr.addstr("Game over!\n")
+    stdscr.addstr(f"Score: {score}\n\n")
+    stdscr.addstr("Press any key to exit...")
+    stdscr.getch()
+
+    # TODO: Add some way of displaying controls
+    # TODO: For actual game, make window fixed size so you can't cheat by making the terminal window bigger (just don't
+    #  use LINES or COLS variables)
+    # TODO: Allow option of borders on or off, i.e. to end game or just wrap around (respectively) when snake reaches
+    #  edge of the screen
+    # TODO: Finish commenting the code
+
+
+def show_title_screen(stdscr):
     stdscr.addstr(" ____              _        \n"
                   "/ ___| _ __   __ _| | _____ \n"
                   "\\___ \\| '_ \\ / _` | |/ / _ \\\n"
@@ -25,84 +47,79 @@ def main(stdscr):
                   "Press any key to start...")
     stdscr.getch()
 
-    curses.curs_set(0)
 
+def show_game_screen(stdscr):
     snake = Snake(stdscr.getmaxyx()[0], stdscr.getmaxyx()[1])
     pellet = (randint(0, stdscr.getmaxyx()[0] - 1), randint(0, stdscr.getmaxyx()[1] - 1))
 
     key = None
-    game_over = False
     # getch return value of 27 corresponds to escape key - doesn't look like curses has a constant for this
-    while key != 27 and key != ord("q") and not game_over:
+    while key != 27 and key != ord("q") and len(snake.cells) == len(set(snake.cells)):
         stdscr.timeout(1000 // len(snake.cells))
         key = stdscr.getch()
 
-        if key == curses.KEY_LEFT:
-            new_direction = (0, -1)
-        elif key == curses.KEY_RIGHT:
-            new_direction = (0, 1)
-        elif key == curses.KEY_UP:
-            new_direction = (-1, 0)
-        elif key == curses.KEY_DOWN:
-            new_direction = (1, 0)
-        else:
-            new_direction = snake.direction
+        pellet = update_game_screen(stdscr, key, snake, pellet)
 
-        new_direction_reversed = (-new_direction[0], -new_direction[1])
-        if snake.direction != new_direction_reversed:
-            snake.direction = new_direction
-
-        current_front = snake.cells[0]
-        # TODO: Use vector library
-        new_front = ((current_front[0] + snake.direction[0]) % stdscr.getmaxyx()[0],
-                     (current_front[1] + snake.direction[1]) % stdscr.getmaxyx()[1])
-        snake.cells.insert(0, new_front)
-
-        if pellet in snake.cells:
-            pellet = (randint(0, stdscr.getmaxyx()[0] - 1), randint(0, stdscr.getmaxyx()[1] - 1))
-        else:
-            snake.cells.pop()
-
-        game_over = len(snake.cells) != len(set(snake.cells))
-
-        stdscr.clear()
-
-        stdscr.addstr(0, 0, f"Score: {len(snake.cells)}")
-
-        if len(snake.cells) <= 3:
-            stdscr.attron(curses.A_STANDOUT)
-            message = "Hint: To move faster, repeatedly press or hold the arrow key."
-            stdscr.addstr(0, stdscr.getmaxyx()[1] - len(message), message)
-            stdscr.attroff(curses.A_STANDOUT)
-
-        for cell in snake.cells:
-            if curses.has_colors():
-                stdscr.attron(curses.color_pair(1))
-            stdscr.addch(cell[0], cell[1], "x")
-            if curses.has_colors():
-                stdscr.attroff(curses.color_pair(1))
-
-        if curses.has_colors():
-            stdscr.attron(curses.color_pair(2))
-        stdscr.addch(pellet[0], pellet[1], "o")
-        if curses.has_colors():
-            stdscr.attroff(curses.color_pair(2))
+        draw_game_screen(pellet, snake, stdscr)
 
     stdscr.nodelay(False)
-    curses.curs_set(1)
 
+    return len(snake.cells)
+
+
+def update_game_screen(stdscr, key, snake, pellet):
+    if key == curses.KEY_LEFT:
+        new_direction = (0, -1)
+    elif key == curses.KEY_RIGHT:
+        new_direction = (0, 1)
+    elif key == curses.KEY_UP:
+        new_direction = (-1, 0)
+    elif key == curses.KEY_DOWN:
+        new_direction = (1, 0)
+    else:
+        new_direction = snake.direction
+
+    new_direction_reversed = (-new_direction[0], -new_direction[1])
+    if snake.direction != new_direction_reversed:
+        snake.direction = new_direction
+
+    current_front = snake.cells[0]
+    # TODO: Use vector library
+    new_front = ((current_front[0] + snake.direction[0]) % stdscr.getmaxyx()[0],
+                 (current_front[1] + snake.direction[1]) % stdscr.getmaxyx()[1])
+    snake.cells.insert(0, new_front)
+
+    if pellet in snake.cells:
+        pellet = (randint(0, stdscr.getmaxyx()[0] - 1), randint(0, stdscr.getmaxyx()[1] - 1))
+    else:
+        snake.cells.pop()
+
+    return pellet
+
+
+def draw_game_screen(pellet, snake, stdscr):
     stdscr.clear()
-    stdscr.addstr("Game over!\n")
-    stdscr.addstr(f"Score: {len(snake.cells)}\n\n")
-    stdscr.addstr("Press any key to exit...")
-    stdscr.getch()
 
-    # TODO: Add some way of displaying controls
-    # TODO: For actual game, make window fixed size so you can't cheat by making the terminal window bigger (just don't
-    #  use LINES or COLS variables)
-    # TODO: Allow option of borders on or off, i.e. to end game or just wrap around (respectively) when snake reaches
-    #  edge of the screen
-    # TODO: Finish commenting the code
+    stdscr.addstr(0, 0, f"Score: {len(snake.cells)}")
+
+    if len(snake.cells) <= 3:
+        stdscr.attron(curses.A_STANDOUT)
+        message = "Hint: To move faster, repeatedly press or hold the arrow key."
+        stdscr.addstr(0, stdscr.getmaxyx()[1] - len(message), message)
+        stdscr.attroff(curses.A_STANDOUT)
+
+    for cell in snake.cells:
+        if curses.has_colors():
+            stdscr.attron(curses.color_pair(1))
+        stdscr.addch(cell[0], cell[1], "x")
+        if curses.has_colors():
+            stdscr.attroff(curses.color_pair(1))
+
+    if curses.has_colors():
+        stdscr.attron(curses.color_pair(2))
+    stdscr.addch(pellet[0], pellet[1], "o")
+    if curses.has_colors():
+        stdscr.attroff(curses.color_pair(2))
 
 
 if __name__ == "__main__":
